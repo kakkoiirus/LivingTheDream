daa1d1cb8a392d41a4bf205702aad966de51086c
Valentin Bagadin <bagadin@dfsystems.ru>
2023-02-27 21:51:26 +0500

    fix header styling

--- components/header.js
+++ components/header.js
@@ -4,8 +4,8 @@ export default function Header() {
 
   return (
     <header className="p-12">
-      <nav className="">
-        <h1 className="text-2xl font-black text-center text-transparent md:text-3xl bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text">
+      <nav className="grid place-items-center">
+        <h1 className="text-2xl font-black text-center text-transparent md:text-3xl bg-gradient-to-r from-fuchsia-400 to-purple-600 bg-clip-text">
           <Link href="/">
             living the dream
           </Link>

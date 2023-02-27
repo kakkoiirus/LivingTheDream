e3de70f76ea0968cafe784ac5edc4080af21e216
Valentin Bagadin <bagadin@dfsystems.ru>
2023-02-27 21:36:09 +0500

    upgrade to next13

--- components/header.js
+++ components/header.js
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header className="p-12">
       <nav className="">
-        <h1 className="text-center text-2xl md:text-3xl font-black bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">
+        <h1 className="text-2xl font-black text-center text-transparent md:text-3xl bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text">
           <Link href="/">
-            <a className="">living the dream</a>
+            living the dream
           </Link>
         </h1>
       </nav>
--- components/postCard.js
+++ components/postCard.js
@@ -3,20 +3,24 @@ import Image from 'next/image';
 
 export default function PostCard({ slug, data }) {
   return (
-      <Link href={`/${encodeURIComponent(slug)}`} prefetch={false}>
-        <a
-          className="relative flex flex-col items-center justify-center w-full p-8 overflow-hidden bg-cover aspect-square bg-gradient-to-r from-stone-100 via-stone-200 to-stone-300 rounded-2xl"
-        >
+      <Link
+        href={`/${encodeURIComponent(slug)}`}
+        passHref
+        prefetch={false}
+        className="relative flex flex-col items-center justify-center w-full p-8 overflow-hidden bg-cover aspect-square bg-gradient-to-r from-stone-100 via-stone-200 to-stone-300 rounded-2xl"
+      >   
           <Image
             src={`/${slug}/images/${data.coverImage}`}
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1024px) 50vw,
+              33vw"
+            style={{ objectFit: 'cover' }}
             alt={data.title}
           />
           <span className="absolute z-20 p-4 m-8 text-xl font-bold md:text-2xl bg-slate-50">
             {data.title}
           </span>
-        </a>
       </Link>
     )
 }
